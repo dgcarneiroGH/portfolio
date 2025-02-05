9499585736c94f0ba7f4151abf406e17d3fb4753
Diego Carneiro <dgcarneiro93@gmail.com>
2025-02-05 21:32:38 +0100

    About component created

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { TagCanvasModule, TagCanvasOptions } from 'ng-tagcanvas';
-import { AnimateComponent } from '../../animate/animate.component';
-import { ITag } from '../../../interfaces';
-import {
-  SocialMediaProfiles,
-  TAGS,
-  TAG_CANVAS_OPTIONS
-} from '../../../constants';
+import { PictureComponent } from '../../shared/picture/picture.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TagCanvasModule],
+  imports: [PictureComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent extends AnimateComponent implements OnInit {
-  linkedin = SocialMediaProfiles.filter(
-    (x) => x.title.toLowerCase().trim() == 'linkedin'
-  )[0];
+export class AboutComponent implements OnInit {
+  // linkedin = SocialMediaProfiles.filter(
+  //   (x) => x.title.toLowerCase().trim() == 'linkedin'
+  // )[0];
 
-  options: TagCanvasOptions = TAG_CANVAS_OPTIONS;
+  // options: TagCanvasOptions = TAG_CANVAS_OPTIONS;
   screenWidth!: number;
-  tags: ITag[] = TAGS;
+  // tags: ITag[] = TAGS;
 
   ngOnInit(): void {
     this.screenWidth = window.innerWidth;
---
+++ src/app/components/shared/picture/picture.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-picture',
+  imports: [CommonModule],
+  templateUrl: './picture.component.html',
+  styleUrl: './picture.component.scss'
+})
+export class PictureComponent implements OnInit {
+  @Input() imgSrc!: string;
+
+  animationDelay: string = '0s';
+
+  ngOnInit(): void {
+    this.animationDelay = `${Math.random() * 3}s`;
+  }
+}
