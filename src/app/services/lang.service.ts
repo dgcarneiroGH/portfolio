58fead4bc5483282b6cd3ec1372d5092eb0d69ce
Diego Carneiro <dgcarneiro93@gmail.com>
2025-04-02 22:21:55 +0200

    Change arrows to custom ones

--- src/app/services/lang.service.ts
+++ src/app/services/lang.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LangService {
+  // private _translate = inject(TranslateService);
+
   private defaultLang = 'es-ES'; // Idioma predeterminado
 
-  constructor(private translate: TranslateService) {
-    this.translate.addLangs(['en-US', 'es-ES']); // Agrega los idiomas disponibles
-    this.translate.setDefaultLang(this.defaultLang);
-
-    const browserLang =
-      localStorage.getItem('language') || this.translate.getBrowserLang();
-    this.setLanguage(
-      browserLang?.match(/en-US|es-ES/) ? browserLang : this.defaultLang
-    );
+  constructor() {
+    // this._translate.addLangs(['en-US', 'es-ES']); // Agrega los idiomas disponibles
+    // this._translate.setDefaultLang(this.defaultLang);
+    // const browserLang =
+    //   localStorage.getItem('language') || this._translate.getBrowserLang();
+    // this.setLanguage(
+    //   browserLang?.match(/en-US|es-ES/) ? browserLang : this.defaultLang
+    // );
   }
 
   setLanguage(lang: string) {
-    this.translate.use(lang);
-    localStorage.setItem('language', lang);
+    // this._translate.use(lang);
+    // localStorage.setItem('language', lang);
   }
 
   getLanguage(): string {
-    return this.translate.currentLang;
+    // return this._translate.currentLang;
+    return '';
   }
 }
