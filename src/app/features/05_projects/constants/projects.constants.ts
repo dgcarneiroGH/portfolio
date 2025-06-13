e0b0139b83395b43addc2824fb8b5172d098ff04
Diego Carneiro <dcarneiro@clarcat.com>
2025-06-13 16:45:50 +0200

    Add new project entries and update descriptions in English and Spanish; include new images for Babé, Datalaia, and Mariskito.

--- src/app/features/05_projects/constants/projects.constants.ts
+++ src/app/features/05_projects/constants/projects.constants.ts
@@ -2,32 +2,47 @@ import { Project } from '../interfaces/project.interface';
 
 export const PROJECTS: Project[] = [
   {
+    year: 2023,
+    coverImgSrc: 'assets/images/proyects/babe.png',
+    name: 'Babé y CIA',
+    description: '05_PROJECTS.DESCRIPTION_BABE'
+  },
+  {
+    year: 2023,
+    coverImgSrc: 'assets/images/proyects/datalaia.png',
+    name: 'Datalaia',
+    description: '05_PROJECTS.DESCRIPTION_DATALAIA'
+  },
+  {
+    year: 2022,
+    coverImgSrc: 'assets/images/proyects/mariskito.jpg',
+    name: 'Mariskito',
+    description: '05_PROJECTS.DESCRIPTION_MARISKITO',
+    url: 'https://mariskito.com/'
+  },
+  {
     year: 2021,
     coverImgSrc: 'assets/images/proyects/pbcinteract.png',
     name: 'PBC Interact',
-    description: '05_PROJECTS.DESCRIPTION_INTERACT',
-    url: 'https://pbcinteract.com/'
+    description: '05_PROJECTS.DESCRIPTION_INTERACT'
   },
   {
     year: 2024,
     coverImgSrc: 'assets/images/proyects/laligabares.png',
     name: 'LALIGA Bares',
-    description: '05_PROJECTS.DESCRIPTION_LALIGA',
-    url: 'https://laligabares.my.salesforce.com/'
+    description: '05_PROJECTS.DESCRIPTION_LALIGA'
   },
   {
     year: 2022,
     coverImgSrc: 'assets/images/proyects/discamino.png',
     name: 'Discamino',
-    description: '05_PROJECTS.DESCRIPTION_DISCAMINO',
-    url: 'https://team.discamino.org/#/login'
+    description: '05_PROJECTS.DESCRIPTION_DISCAMINO'
   },
   {
     year: 2022,
     coverImgSrc: 'assets/images/proyects/pharmachallenge.png',
     name: 'Pharma Challenge',
-    description: '05_PROJECTS.DESCRIPTION_PHARMA',
-    url: 'https://pharmachallenge.es/'
+    description: '05_PROJECTS.DESCRIPTION_PHARMA'
   },
   {
     year: 2022,
@@ -38,8 +53,8 @@ export const PROJECTS: Project[] = [
   },
   {
     year: 2025,
-    coverImgSrc: 'assets/images/proyects/hermes.jpg',
-    name: 'Hermes',
+    coverImgSrc: 'assets/images/proyects/arcelor.png',
+    name: 'Hermes - Artemis',
     description: '05_PROJECTS.DESCRIPTION_HERMES'
   }
 ];
