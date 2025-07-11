8a94781f489dedaaa4d9e269c1243d352e80c6e4
Diego Carneiro <dgcarneiro93@gmail.com>
2025-07-11 12:12:02 +0200

    Agregar información de contacto y tooltip en el componente de contacto. Se actualizan los estilos para incluir un efecto de hover en los iconos de contacto y se mejora la estructura del HTML. Se añade un nuevo campo "info" en la interfaz y constantes de contacto.

--- src/app/features/contact/constants/contact.constants.ts
+++ src/app/features/contact/constants/contact.constants.ts
@@ -4,16 +4,19 @@ export const CONTACT_METHODS: ContactMethod[] = [
   {
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/in/dgcarneiro/',
-    iconSrc: 'assets/images/icons/linkedin.svg'
+    iconSrc: 'assets/images/icons/linkedin.svg',
+    info: 'dgcarneiro'
   },
   {
     name: 'Gmail',
     url: 'mailto:dgcarneiro93@gmail.com',
-    iconSrc: 'assets/images/icons/gmail.svg'
+    iconSrc: 'assets/images/icons/gmail.svg',
+    info: 'dgcarneiro93@gmail.com'
   },
   {
     name: 'Phone',
     url: 'tel:+34661206962',
-    iconSrc: 'assets/images/icons/phone.svg'
+    iconSrc: 'assets/images/icons/phone.svg',
+    info: '+34 661 206 962'
   }
 ];
--- src/app/features/contact/interfaces/contact.interface.ts
+++ src/app/features/contact/interfaces/contact.interface.ts
@@ -2,4 +2,5 @@ export interface ContactMethod {
   name: string;
   url: string;
   iconSrc: string;
+  info: string;
 }
