f4c2f90275e8c0b889a994e8af7b3ad5cfcabff1
Diego Carneiro <clarcat.ti@clarcat.com>
2025-10-02 15:10:49 +0200

    Add certificates to home page. Development still pending
    Delete unused images

--- src/app/features/certs/constants/certs.constants.ts
+++ src/app/features/certs/constants/certs.constants.ts
@@ -6,51 +6,51 @@ export const CERTS: Cert[] = [
     platform: 'scrum',
     technology: 'scrum',
     name: 'Scrum Developer I',
-    iconSrc: 'assets/images/icons/certs/PSDI',
+    iconSrc: 'assets/images/certs/PSDI.avif',
     url: 'https://www.scrum.org/certificates/899749'
-  },
-  {
-    platform: 'udemy',
-    technology: 'solid',
-    name: 'Principios SOLID y Clean Code',
-    iconSrc: 'assets/images/icons/certs/udemy',
-    url: 'https://www.udemy.com/certificate/UC-a6d0025a-e9fe-464b-a1ad-52f625ecdde7/'
-  },
-  {
-    platform: 'udemy',
-    technology: 'angular',
-    name: 'REDUX en Angular con NGRX: Desde las bases hasta la práctica',
-    iconSrc: 'assets/images/icons/certs/udemy_angular',
-    url: 'https://www.udemy.com/certificate/UC-d2ceb6dc-f5ab-49aa-b3db-f1c8d16338f8/'
-  },
-  {
-    platform: 'udemy',
-    technology: 'angular',
-    name: 'Angular Avanzado: Lleva tus bases al siguiente nivel - MEAN',
-    iconSrc: 'assets/images/icons/certs/udemy_angular',
-    url: 'https://www.udemy.com/certificate/UC-063523e7-fb90-4dfc-a8a8-d0e22d414384/'
-  },
-  {
-    platform: 'udemy',
-    technology: 'flutter',
-    name: 'Flutter: Tu guía completa de desarrollo para IOS y Android',
-    iconSrc: 'assets/images/icons/certs/udemy_flutter',
-    url: 'https://www.udemy.com/certificate/UC-9202264e-14c0-4636-97cc-a7424cd58433/'
-  },
-  {
-    platform: 'udemy',
-    technology: 'unreal',
-    name: 'Programar Blueprints en Unreal Engine de 0 a profesional',
-    iconSrc: 'assets/images/icons/certs/udemy_unreal',
-    url: 'https://www.udemy.com/certificate/UC-d4cfc152-cd74-413c-b30f-b842f442ab9e/'
-  },
-  {
-    platform: 'udemy',
-    technology: 'react',
-    name: 'React y Firebase: El Curso Completo, Práctico y Desde 0',
-    iconSrc: 'assets/images/icons/certs/udemy_react',
-    url: 'https://www.udemy.com/certificate/UC-deae80fd-4fdc-42b3-aa37-6c232c2245cd/'
   }
+  // {
+  //   platform: 'udemy',
+  //   technology: 'solid',
+  //   name: 'Principios SOLID y Clean Code',
+  //   iconSrc: 'assets/images/icons/certs/udemy',
+  //   url: 'https://www.udemy.com/certificate/UC-a6d0025a-e9fe-464b-a1ad-52f625ecdde7/'
+  // },
+  // {
+  //   platform: 'udemy',
+  //   technology: 'angular',
+  //   name: 'REDUX en Angular con NGRX: Desde las bases hasta la práctica',
+  //   iconSrc: 'assets/images/icons/certs/udemy_angular',
+  //   url: 'https://www.udemy.com/certificate/UC-d2ceb6dc-f5ab-49aa-b3db-f1c8d16338f8/'
+  // },
+  // {
+  //   platform: 'udemy',
+  //   technology: 'angular',
+  //   name: 'Angular Avanzado: Lleva tus bases al siguiente nivel - MEAN',
+  //   iconSrc: 'assets/images/icons/certs/udemy_angular',
+  //   url: 'https://www.udemy.com/certificate/UC-063523e7-fb90-4dfc-a8a8-d0e22d414384/'
+  // },
+  // {
+  //   platform: 'udemy',
+  //   technology: 'flutter',
+  //   name: 'Flutter: Tu guía completa de desarrollo para IOS y Android',
+  //   iconSrc: 'assets/images/icons/certs/udemy_flutter',
+  //   url: 'https://www.udemy.com/certificate/UC-9202264e-14c0-4636-97cc-a7424cd58433/'
+  // },
+  // {
+  //   platform: 'udemy',
+  //   technology: 'unreal',
+  //   name: 'Programar Blueprints en Unreal Engine de 0 a profesional',
+  //   iconSrc: 'assets/images/icons/certs/udemy_unreal',
+  //   url: 'https://www.udemy.com/certificate/UC-d4cfc152-cd74-413c-b30f-b842f442ab9e/'
+  // },
+  // {
+  //   platform: 'udemy',
+  //   technology: 'react',
+  //   name: 'React y Firebase: El Curso Completo, Práctico y Desde 0',
+  //   iconSrc: 'assets/images/icons/certs/udemy_react',
+  //   url: 'https://www.udemy.com/certificate/UC-deae80fd-4fdc-42b3-aa37-6c232c2245cd/'
+  // }
 ];
 
 export const CERTS_FILTER: CertFilter[] = [
--- src/app/features/home/components/home.component.ts
+++ src/app/features/home/components/home.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { OscillatorComponent } from 'src/app/shared/components/oscillator/oscillator.component';
 import { AnimateDirective } from 'src/app/shared/directives/animate.directive';
+import { CERTS } from '../../certs/constants/certs.constants';
+import { Cert } from '../../certs/interfaces/certs.interface';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [TranslateModule, AnimateDirective, OscillatorComponent]
+  imports: [
+    TranslateModule,
+    AnimateDirective,
+    OscillatorComponent,
+    CommonModule
+  ]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  certs: Cert[] = CERTS;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    const logos = this.el.nativeElement.querySelectorAll('.cert-img');
+    logos.forEach((logo: HTMLElement, index: number) => {
+      console.log({ logo });
+      console.log({ index });
+
+      // Posición aleatoria dentro del viewport
+      const top = Math.random() * 80 + 10; // entre 10% y 90%
+      const left = Math.random() * 80 + 10; // entre 10% y 90%
+
+      logo.style.top = `${top}vh`;
+      logo.style.left = `${left}vw`;
+
+      // Delay incremental (aparecen uno tras otro)
+      logo.style.animationDelay = `${index + 2}s`;
+    });
+  }
+}
