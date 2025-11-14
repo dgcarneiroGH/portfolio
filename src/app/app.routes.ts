3e0149d7cc68d7d32608209731d053b688875025
Diego Carneiro <dgcarneiro93@gmail.com>
2025-11-14 16:47:17 +0100

    Add TODO for styling 404 page in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -34,5 +34,10 @@ export const routes: Routes = [
   {
     path: '**',
     redirectTo: ''
+    //TODO:Dale estilo al 404, puedes usar la imagen que ya tienes para el blog, lo mismo con el texto
+    // loadComponent: () =>
+    //   import('./core/not-found-404/not-found-404.component').then(
+    //     (m) => m.NotFound404Component
+    //   )
   }
 ];
