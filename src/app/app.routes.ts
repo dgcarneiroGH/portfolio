d6f0b8a15be60cbb33589fc7d4a133af91bb05ea
Diego Carneiro <dgcarneiro93@gmail.com>
2025-11-14 16:41:59 +0100

    Restore BlogComponent in app.routes.ts and uncomment blog button in sections-wrapper.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,14 +12,14 @@ export const routes: Routes = [
   },
   {
     path: 'blog',
-    // loadComponent: () =>
-    //   import('./features/blog/components/blog.component').then(
-    //     (m) => m.BlogComponent
-    //   ),
     loadComponent: () =>
-      import(
-        './core/components/features/blog/blog-test/blog-test.component'
-      ).then((m) => m.BlogTestComponent),
+      import('./features/blog/components/blog.component').then(
+        (m) => m.BlogComponent
+      ),
+    // loadComponent: () =>
+    //   import(
+    //     './core/components/features/blog/blog-test/blog-test.component'
+    //   ).then((m) => m.BlogTestComponent),
     data: { animation: 'Blog' }
   },
   {
